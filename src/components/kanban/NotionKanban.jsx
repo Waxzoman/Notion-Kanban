62df751a16179e08e5040a06331bd9e2220a3398
Waxzoman <omar.morsy.yakout@gmail.com>
2024-02-13 23:38:36 +0100

    created mock data to render the kanban

--- src/components/kanban/NotionKanban.jsx
+++ src/components/kanban/NotionKanban.jsx
@@ -91,3 +91,69 @@ const Column = ({ title, headingColor, color, cards, setCards }) => {
         </div>
     )
 };
+
+
+const DEFAULT_CARDS = [
+    //BACKLOG
+    {
+        title: "Loook into render bug in dashboard",
+        id: "1",
+        column: "backlog"
+    },
+    {
+        title: "SOX complaince checklist",
+        id: "2",
+        column: "backlog"
+    },
+    {
+        title: "[SPIKE] Migrate to Azure",
+        id: "3",
+        column: "backlog"
+    },
+    {
+        title: "Document Notifiations service",
+        id: "4",
+        column: "backlog"
+    },
+    //TODO
+   {
+
+    title: "Research DB options for new microservice",
+
+    id: "5",
+
+    column: "todo",
+
+  },
+
+  { title: "Postmortem for outage", id: "6", column: "todo" },
+
+  { title: "Sync with product on Q3 roadmap", id: "7", column: "todo" },
+
+
+  // DOING
+
+  {
+
+    title: "Refactor context providers to use Zustand",
+
+    id: "8",
+
+    column: "doing",
+
+  },
+
+  { title: "Add logging to daily CRON", id: "9", column: "doing" },
+
+  // DONE
+
+  {
+
+    title: "Set up DD dashboards for Lambda listener",
+
+    id: "10",
+
+    column: "done",
+
+  },
+]
